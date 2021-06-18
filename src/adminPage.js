e833f08eb7b90ee9256cea80936c82919d8a09eb
CHRONOS-DEVELOPER <chronosdevtutors1032000@gmail.com>
2021-06-18 14:08:30 -0300

    update adminpage

--- src/adminPage.js
+++ src/adminPage.js
@@ -15,14 +15,16 @@ function ADDaDVD() {
     if (MusicName != "" && MusicGender != "" && MusicAuthor != "") {
         Music.songs.forEach((item, indice) => {
             if (item.name == MusicName && item.author == MusicAuthor && item.gender == MusicGender) {
+                audio.play()
                 alert("A MÚSICA " + MusicName + " JÁ EXISTE!!");
-                Musicconfirm = false;
+
+                window.Musicconfirm = false;
             } else {
-                Musicconfirm = true;
+                window.Musicconfirm = true;
             }
 
         });
-        if (Musicconfirm == true) {
+        if (Musicconfirm === true) {
             Music.gender[MusicGender].songs.push(MusicName);
             Music.songs.push({ "name": MusicName, "author": MusicAuthor, "gender": MusicGender })
             localStorage.Music = JSON.stringify(Music);
@@ -57,32 +59,64 @@ function ADDaAUTHORlist() {
 var SEARCHthisDVD = {
 
     "SearchName": function() {
-        var MusicAuthor = document.getElementById('MusicAuthor').value;
-        var MusicGender = document.getElementById('MusicGender').value;
         var MusicName = document.getElementById('MusicName').value;
-
         if (MusicName != "") {
+            playconfirm = 0
+            Screenmain.innerHTML = "<h1 class='text-info lg'>Sound Paradise</h1> <br/><h1 class='text-info sm'> Buscar Por Música</h1> <br/> <br/><div class='table-overflow'><table class='table table-lg table-striped table-dark'><thead><tr><th scope = 'col'>ID#</th><th scope = 'col'>Nome da Música</th><th scope = 'col'>Cantor</th><th scope = 'col'>Gênero</th></tr></thead><tbody id='tablebody'></tbody></table>"
+            Music.songs.forEach((item, indice) => {
+                if (item.name == MusicName) {
+                    playconfirm++
+                    $('#tablebody').append("<tr><th scope='row' id='tableid'>" + indice + "</th><td id='tablename'>" + item.name + "</td><td id='tableauthor'>" + item.author + "</td><td id='tablegender'>" + item.gender + "</td></tr>")
+                }
 
+            });
+            if (playconfirm == 0) {
+
+                audio.play()
+                alert("ESTA MÚSICA NÃO EXISTE")
+
+            }
         }
     },
     "SearchGender": function() {
-        var MusicAuthor = document.getElementById('MusicAuthor').value;
         var MusicGender = document.getElementById('MusicGender').value;
-        var MusicName = document.getElementById('MusicName').value;
-
         if (MusicGender != "SELECIONE O GÊNERO") {
+            Screenmain.innerHTML = "<h1 class='text-info lg'>Sound Paradise</h1> <br/><h1 class='text-info sm'> Buscar Por Música</h1> <br/> <br/><div class='table-overflow'><table class='table table-lg table-striped table-dark'><thead><tr><th scope = 'col'>ID#</th><th scope = 'col'>Nome da Música</th><th scope = 'col'>Cantor</th><th scope = 'col'>Gênero</th></tr></thead><tbody id='tablebody'></tbody></table>"
+            Music.songs.forEach((item, indice) => {
+                if (item.gender == MusicGender) {
 
+                    $('#tablebody').append("<tr><th scope='row' id='tableid'>" + indice + "</th><td id='tablename'>" + item.name + "</td><td id='tableauthor'>" + item.author + "</td><td id='tablegender'>" + item.gender + "</td></tr>")
+                }
+
+            });
         }
     },
     "SearchAuthor": function() {
         var MusicAuthor = document.getElementById('MusicAuthor').value;
-        var MusicGender = document.getElementById('MusicGender').value;
-        var MusicName = document.getElementById('MusicName').value;
-
         if (MusicAuthor != "SELECIONE O CANTOR") {
 
+            Screenmain.innerHTML = "<h1 class='text-info lg'>Sound Paradise</h1> <br/><h1 class='text-info sm'> Buscar Por Música</h1> <br/> <br/><div class='table-overflow'><table class='table table-lg table-striped table-dark'><thead><tr><th scope = 'col'>ID#</th><th scope = 'col'>Nome da Música</th><th scope = 'col'>Cantor</th><th scope = 'col'>Gênero</th></tr></thead><tbody id='tablebody'></tbody></table>"
+            Music.songs.forEach((item, indice) => {
+                if (item.author == MusicAuthor) {
+
+                    $('#tablebody').append("<tr><th scope='row' id='tableid'>" + indice + "</th><td id='tablename'>" + item.name + "</td><td id='tableauthor'>" + item.author + "</td><td id='tablegender'>" + item.gender + "</td></tr>")
+                }
+
+            });
+
         }
+    },
+    "SearchAll": function() {
+        Screenmain.innerHTML = "<h1 class='text-info lg'>Sound Paradise</h1> <br/><h1 class='text-info sm'> Buscar Por Música</h1> <br/> <br/><div class='table-overflow'><table class='table table-lg table-striped table-dark'><thead><tr><th scope = 'col'>ID#</th><th scope = 'col'>Nome da Música</th><th scope = 'col'>Cantor</th><th scope = 'col'>Gênero</th></tr></thead><tbody id='tablebody'></tbody></table>"
+        Music.songs.forEach((item, indice) => {
+
+
+            $('#tablebody').append("<tr><th scope='row' id='tableid'>" + indice + "</th><td id='tablename'>" + item.name + "</td><td id='tableauthor'>" + item.author + "</td><td id='tablegender'>" + item.gender + "</td></tr>")
+
+
+        });
     }
+
 
 }
 
@@ -90,6 +124,9 @@ function SEARCHaDVD() {
 
     Screenmain.innerHTML = "<h1 class='text-info lg'>Sound Paradise</h1> <br/><h1 class='text-info sm'> Consultar Música</h1> <br/> <br/><input type='text' class='form-control form-control-lg' id='MusicName' placeholder='NOME DA MUSICA'><button class='btn  btn-info' onclick='SEARCHthisDVD.SearchName()'>PESQUISAR POR NOME</button><br/><br/><select class='form-control form-control-lg' id='MusicGender'><option>SELECIONE O GÊNERO</option></select><button class='btn  btn-info' onclick='SEARCHthisDVD.SearchGender()'>PESQUISAR POR GÊNERO</button><br/><br/><select class='form-control form-control-lg' id='MusicAuthor'><option>SELECIONE O CANTOR</option></select><button class='btn btn-info' onclick='SEARCHthisDVD.SearchAuthor()'>PESQUISAR POR AUTOR</button><button class='btn btn-block btn-info' onclick='SEARCHthisDVD.SearchAll()'>LISTA COMPLETA DE DVDs</button>"
     ADDaGENDERlist()
+    Music.authors.forEach(element => {
+        $('#MusicAuthor').append('<option>' + element + '</option>');
+    });
 
 }
 
